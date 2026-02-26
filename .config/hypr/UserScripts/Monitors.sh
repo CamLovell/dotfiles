adfa3b06306f36272d067cd0d20acaed91e6bfff
Cameron Lovell <cameron.e.lovell@gmail.com>
2026-02-27 09:49:10 +1100

    Add kooldots ready for mods

---
+++ .config/hypr/UserScripts/Monitors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+MONITORS_CONFIG=$HOME/.config/hypr/monitor_definitions.conf
+
+set -e
+echo $MONITORS_CONFIG
+mapfile -t monitors < <(
+    hyprctl monitors all |
+    awk '/Monitor DP-/{name=$2; id=$4; print id, name}' |
+    sort -n |
+    awk '{print $2}'
+
+)
+
+echo "# Auto-generated monitor definitsions by UserScripts/Monitors.sh" > "$MONITORS_CONFIG"
+echo "\$laptop = eDP-1" >> "$MONITORS_CONFIG"
+echo "\$horizontal = ${monitors[0]}" >> "$MONITORS_CONFIG"
+echo "\$vertical = ${monitors[1]}" >> "$MONITORS_CONFIG"
+
+LID_STATE=$(awk -F': *' '{print $2}' /proc/acpi/button/lid/LID0/state)
+if [ $LID_STATE = 'closed' ]; then
+    echo "\$lid_shut = true" >> "$MONITORS_CONFIG"
+fi
+
